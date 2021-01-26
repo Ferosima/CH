e3b8fd4b371cee61353f66844c2ca08eeaf6dd0e
Гусаченко Сергій <gusachenko02@gmail.com>
2021-01-26 22:04:56 +0200

    update gitignore

--- android/app/src/main/java/com/ch_alpha/MainApplication.java
+++ android/app/src/main/java/com/ch_alpha/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import co.apptailor.googlesignin.RNGoogleSigninPackage; 
 // import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           // packages.add(new ReactNativeFirebaseAppPackage());
+          packages.add( new RNGoogleSigninPackage());
           return packages;
         }
 
